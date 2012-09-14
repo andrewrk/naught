0f4b9fbd5a0c60145de885e1f8525a14354e171a
Andrew Kelley <superjoe30@gmail.com>
2012-09-14 12:31:02 -0400

    4 layer execution. main -> daemon -> master -> workers

--- test/simple/server.js
+++ test/simple/server.js
@@ -2,10 +2,13 @@ var http;
 
 http = require("http");
 
+console.log("stdout: attempting to listen");
 http.createServer(function(req, resp) {
   if (req.path === "/die") {
     throw new Error("unhandled exception");
   } else {
     resp.end("hi");
   }
-}).listen(11904);
+}).listen(11904, function() {
+  console.error("Stderr: listening"); 
+});
