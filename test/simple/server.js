b63b92590f7812b66e890fbd240c06efac19ce75
Andrew Kelley <superjoe30@gmail.com>
2012-09-17 16:00:11 -0400

    ability to abort deploy

--- test/simple/server.js
+++ test/simple/server.js
@@ -6,8 +6,17 @@ console.log("stdout: attempting to listen");
 http.createServer(function(req, resp) {
   if (req.path === "/die") {
     throw new Error("unhandled exception");
+  } else if (req.path === "/event") {
+    req.socket.setTimeout(Infinity);
+    resp.statusCode = 200;
+    resp.setHeader('Content-Type', 'text/event-stream');
+    resp.setHeader('Cache-Control', 'no-cache');
+    resp.setHeader('Connection', 'keep-alive');
+
+    resp.write("id: 0\n");
+    resp.write("data: 'hi'}\n\n");
   } else {
-    resp.end("hi");
+    resp.end('<html><head></head><body><script>var ev = new EventSource("/event");</script></body></html>');
   }
 }).listen(11904, function() {
   console.error("stderr: listening");
